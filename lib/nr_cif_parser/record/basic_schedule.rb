824b2babc699fa9087a616f58f1ac78878e6819c
Jon Pearse <hello@jonpearse.net>
2019-08-11 17:04:38 +0100

    Updating BS parser to use stricter parsing

--- lib/nr_cif_parser/record/basic_schedule.rb
+++ lib/nr_cif_parser/record/basic_schedule.rb
@@ -15,31 +15,31 @@ module NrCifParser::Record
       
     def self.definition
       {
-        type:           FieldTypes::Char.new,
-        train_uid:      FieldTypes::String.new( 6 ),
+        type:           FieldTypes::Enum.new( %w{ N D R } ),
+        train_uid:      FieldTypes::String.new( 6, false, /[A-Z]\d{5}/ ),
         date_from:      FieldTypes::Date.new,
         date_until:     FieldTypes::Date.new,
         days_run:       FieldTypes::Bit.new( 7 ),
-        bank_holiday:   FieldTypes::Char.new( true ),
-        status:         FieldTypes::Char.new,
-        category:       FieldTypes::String.new( 2 ),
-        identity:       FieldTypes::String.new( 4 ),
+        bank_holiday:   FieldTypes::Enum.new( %w{ E G X }, true ),
+        status:         FieldTypes::Enum.new( %w{ B F P S T 1 2 3 4 5 }),
+        category:       FieldTypes::String.new( 2 ), # @TODO: expand to ENUM
+        identity:       FieldTypes::String.new( 4, false, /\d[A-Z]\d\d/ ),
         headcode:       FieldTypes::Number.new( 4, true ),
         indicator:      FieldTypes::Unused.new( 1 ),
         service_code:   FieldTypes::Number.new( 8 ),
-        portion_id:     FieldTypes::Char.new( true ),
-        power_type:     FieldTypes::String.new( 3, true ),
-        timing_load:    FieldTypes::String.new( 4, true ),
+        portion_id:     FieldTypes::Char.new( true, /[ Z1248]/ ),
+        power_type:     FieldTypes::Enum.new( %w{ D DEM DMU E ED EML EMU HST }),
+        timing_load:    FieldTypes::String.new( 4, true, /\A(A|E|N|S|T|V|X|D[1-3]|\d{1,4})\s*\Z/ ),
         speed:          FieldTypes::Number.new( 3 ),
-        op_character:   FieldTypes::String.new( 6, true ),
-        seating_class:  FieldTypes::Char.new( true ),
-        sleepers:       FieldTypes::Char.new( true ),
-        reservations:   FieldTypes::Char.new( true ),
+        op_character:   FieldTypes::String.new( 6, true, /\A[BCDEGMPQRSYZ]*\s*\Z/ ),
+        seating_class:  FieldTypes::Enum.new( %w{ B S }, true ),
+        sleepers:       FieldTypes::Enum.new( %w{ B F S }, true ),
+        reservations:   FieldTypes::Enum.new( %w{ A E R S }, true ),
         connection:     FieldTypes::Unused.new( 1 ),
-        catering_code:  FieldTypes::String.new( 4, true ),
-        service_brand:  FieldTypes::String.new( 4, true ),
+        catering_code:  FieldTypes::String.new( 4, true, /\A[CFHMRT]*\s*\Z/ ),
+        service_brand:  FieldTypes::String.new( 4, true, /\AE?\s*\Z/ ),
         spare:          FieldTypes::Unused.new( 1 ),
-        stp_indicator:  FieldTypes::Char.new
+        stp_indicator:  FieldTypes::Enum.new( %w{ C N O P })
       }
     end
     
--- test/tests/test_bs_record.rb
+++ test/tests/test_bs_record.rb
@@ -24,37 +24,44 @@ class TestBSRecord < Minitest::Test
     
   end
   
-  def test_invalid_message
-    
-    assert_raises( NrCifParser::RecordParserError ) do
-    
-      arb_parse( 'XX THIS SHOULD FAIL' )
-    
-    end
+  def should_fail( msg )
     
     assert_raises( NrCifParser::RecordParserError ) do
       
-      arb_parse( 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' )
+      arb_parse( msg )
       
     end
     
   end
   
+  def test_invalid_message
+    
+    should_fail 'XX THIS SHOULD FAIL'
+    should_fail 'BSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    
+  end
+  
   def test_transaction_type
   
-    assert_equal "R", record.type
+    assert_equal 'R', record.type
+    assert_equal 'N', arb_parse( 'BSNG828851510191510231100100 POO2N75    113575825 DMUE   090      S            O' ).type
+    should_fail 'BSQG828851510191510231100100 POO2N75    113575825 DMUE   090      S            O' 
   
   end
   
   def test_train_uid
   
     assert_equal "G82885", record.train_uid
+    
+    should_fail 'BSR8828851510191510231100100 POO2N75    113575825 DMUE   090      S            O'
+    should_fail 'BSR88Q8851510191510231100100 POO2N75    113575825 DMUE   090      S            O'
   
   end
   
   def test_date_from
   
     assert_equal Date.new( 2015, 10, 19), record.date_from
+    should_fail 'BSQG828851510A91510231100100 POO2N75    113575825 DMUE   090      S            O' 
   
   end
   
@@ -79,19 +86,16 @@ class TestBSRecord < Minitest::Test
   def test_bank_holiday
   
     assert_nil record.bank_holiday
-    assert_equal "X", arb_parse( 'BSRG828851510191510231100100X' ).bank_holiday
-  
-    # assert_raises( NrCifParser::RecordParserError ) do
-    # 
-    #   NrCifParser::Record::BasicSchedule.parse( 'BSRG828851510191510231100100Q' )
-    # 
-    # end
+    assert_equal "X", arb_parse( 'BSRG828851510191510231100100XPOO2N75    113575825 DMUE   090      S            O' ).bank_holiday
+    should_fail 'BSRG828851510191510231100100QPOO2N75    113575825 DMUE   090      S            O'
   
   end
   
   def test_status
   
     assert_equal "P", record.status
+    should_fail 'BSRG828851510191510231100100Q9OO2N75    113575825 DMUE   090      S            O'
+    should_fail 'BSRG828851510191510231100100QXOO2N75    113575825 DMUE   090      S            O'
   
   end
   
@@ -104,6 +108,10 @@ class TestBSRecord < Minitest::Test
   def test_identity
   
     assert_equal "2N75", record.identity
+    should_fail 'BSRG828851510191510231100100 POO2575    113575825 DMUE   090      S            O'
+    should_fail 'BSRG828851510191510231100100 POOE575    113575825 DMUE   090      S            O'
+    should_fail 'BSRG828851510191510231100100 POO25E5    113575825 DMUE   090      S            O'
+    should_fail 'BSRG828851510191510231100100 POO257E    113575825 DMUE   090      S            O'
   
   end
   
@@ -116,25 +124,37 @@ class TestBSRecord < Minitest::Test
   def test_service_code
   
     assert_equal 13575825, record.service_code
+    should_fail 'BSRG828851510191510231100100 POO2N75    11X575825 DMUE   090      S            O'
   
   end
   
   def test_portion_id
   
     assert_nil record.portion_id
+    assert_equal 'Z', arb_parse( 'BSNG828851510191510231100100 POO2N75    113575825ZDMUE   090      S            O' ).portion_id
+    should_fail 'BSRG828851510191510231100100 POO2N75    11X5758255DMUE   090      S            O'
+    should_fail 'BSRG828851510191510231100100 POO2N75    11X575825XDMUE   090      S            O'
   
   end
   
   def test_power_type
   
-    assert_equal "DMU", record.power_type
+    assert_equal 'DMU', record.power_type
+    assert_equal 'ED', arb_parse( 'BSNG828851510191510231100100 POO2N75    113575825ZED E   090      S            O' ).power_type
+    should_fail 'BSRG828851510191510231100100 POO2N75    11X575825XXX E   090      S            O'
   
   end
   
   def test_timing_load
   
-    assert_equal "E", record.timing_load
-  
+    assert_equal 'E', record.timing_load
+    
+    assert_equal '1234', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMU1234090      S            O' ).timing_load
+    assert_equal 'D3',  arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUD3  090      S            O' ).timing_load
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUF   090      S            O'
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUFX  090      S            O'
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE0  090      S            O'
+    
   end
   
   def test_speed
@@ -146,24 +166,36 @@ class TestBSRecord < Minitest::Test
   def test_operating_characteristics
   
     assert_nil record.op_character
+    
+    assert_equal "BEG", arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUD3  090BEG   S            O' ).op_character
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090XXXXXSS            O'
   
   end
   
   def test_seating_class
   
     assert_equal "S", record.seating_class
+    
+    assert_nil arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090                   O' ).seating_class
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      X            O'
   
   end
   
   def test_sleepers
   
     assert_nil record.sleepers
+    
+    assert_equal 'B', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      SB           O' ).sleepers
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      SX           O'
   
   end
   
   def test_reservations
   
     assert_nil record.reservations
+    
+    assert_equal 'A', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S A          O' ).reservations
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S X          O'
   
   end
   
@@ -171,17 +203,31 @@ class TestBSRecord < Minitest::Test
   
     assert_nil record.catering_code
   
+    assert_equal 'C', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S   C        O' ).catering_code
+    assert_equal 'TF', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S   TF       O' ).catering_code
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S   X        O'
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S   XX       O'
+  
   end
   
   def test_service_brand
   
     assert_nil record.service_brand
+    
+    assert_equal 'E', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S       E    O' ).service_brand
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S       EX   O'
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S       X  X O'
   
   end
   
   def test_stp_indicator
   
     assert_equal "O", record.stp_indicator
+    
+    assert_equal 'C', arb_parse( 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S            C' ).stp_indicator
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S            X'
+    should_fail 'BSRG828851510191510231100100 POO2N75    113575825 DMUE   090      S             '
+    
   
   end
   
